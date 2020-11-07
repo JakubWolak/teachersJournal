d1832d27eb97e2a2f1edb2c13f80a826c292aef3
JakubWolak <jwolak@cryptovoucher.io>
2020-11-07 23:04:36 +0100

    remove comment

--- teachersJournal/settings.py
+++ teachersJournal/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xzs@c@b*s)r3(w!j8k*pt@#_4#oglezai^__$0kq%7p__dv$ci'
